eedf5728f942ede1f50c889204c99577d1c86cc5
JoelVaarala <bgh275@myy.haaga-helia.fi>
2020-02-07 06:47:15 +0200

    projektin alustus

---
+++ Bookstore/src/main/java/palvelinkurssi/Bookstore/web/BooktstoreController.java
@@ -0,0 +1,15 @@
+package palvelinkurssi.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BooktstoreController {
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String frontPage(){
+		return "index";
+	}
+
+}
